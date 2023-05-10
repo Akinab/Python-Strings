fced2eef7fa41bdbacef7f9d14d06a34d215d8e8
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-10 23:16:39 +0800

    Encrypted Text

--- decrypt.py
+++ decrypt.py
@@ -9,3 +9,17 @@ details ()
 def decrypt(encrypted_text):
     decrypted_text = ""
     for char in encrypted_text:
+        if char == '*':
+            decrypted_text += 'a'
+        elif char == '&':
+            decrypted_text += 'e'
+        elif char == '#':
+            decrypted_text += 'i'
+        elif char == '+':
+            decrypted_text += 'o'
+        elif char == '!':
+            decrypted_text += 'u'
+        else:
+            decrypted_text += char
+    return decrypted_text
+encrypted_text = input("Enter the encrypted text to be decrypted: ")
