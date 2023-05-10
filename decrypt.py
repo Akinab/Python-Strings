d74306dbf3dd99d6fda9dba58f44cd6093f6720a
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-10 23:12:40 +0800

    For statement

--- decrypt.py
+++ decrypt.py
@@ -1,9 +1,11 @@
 def details ():
     Description = "Decryption"
     Date = "05-10-23"
-    print ("\nDescription: {}\nDate: {}".format (Description , Date))
+    Plain_Text = "the quick brown fox jumps over the lazy dog."
+    print ("\nDescription: {}\nDate: {}\nPlain_Text: {}".format (Description , Date, Plain_Text))
     
 details ()
 
 def decrypt(encrypted_text):
     decrypted_text = ""
+    for char in encrypted_text:
