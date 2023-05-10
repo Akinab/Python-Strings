aebebd5df36dac03660f97c45fe2c76039ee1ef1
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-10 23:20:57 +0800

    Decrypted Text

--- decrypt.py
+++ decrypt.py
@@ -1,7 +1,7 @@
 def details ():
     Description = "Decryption"
     Date = "05-10-23"
-    Plain_Text = "the quick brown fox jumps over the lazy dog."
+    Plain_Text = "th& q!#ck br+wn f+x j!mps +v&r th& l*zy d+g."
     print ("\nDescription: {}\nDate: {}\nPlain_Text: {}".format (Description , Date, Plain_Text))
     
 details ()
@@ -24,3 +24,4 @@ def decrypt(encrypted_text):
     return decrypted_text
 encrypted_text = input("Enter the encrypted text to be decrypted: ")
 decrypted_text = decrypt(encrypted_text)
+print("Decrypted Text:", decrypted_text)
