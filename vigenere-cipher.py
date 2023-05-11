381dfe5318aeff8a36c1d91f17c0dea1bc9d3bd1
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-11 12:40:46 +0800

    Defining details of the program

---
+++ vigenere-cipher.py
@@ -0,0 +1,7 @@
+def details ():
+    Description = "Decryption"
+    Date = "05-10-23"
+    Plain_Text = "th& q!#ck br+wn f+x j!mps +v&r th& l*zy d+g."
+    print ("\nDescription: {}\nDate: {}\nPlain_Text: {}".format (Description , Date, Plain_Text))
+    
+details ()
